4667ba2371af36f7db5162739fc02267694106d4
KVeronikaR <veronika.cuzmina2011@yandex.ru>
2021-11-19 17:38:00 +0100

    ts component with js tests working

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  moduleNameMapper: {
+    'vuetify/lib(.*)': '<rootDir>/node_modules/vuetify/es5$1',
+    '^.+\\.(css|sass)$': '<rootDir>/tests/__mocks__/scssConfig.ts'
+  }
 }
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,5 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+declare module 'vuetify/lib/components/VIcon/index.js'
